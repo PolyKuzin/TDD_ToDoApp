ef409173a2dfe4a690d0fdf478feabaff27c661d
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-08-24 21:05:01 +0300

    Serialization into dictionary Tests

--- ToDoApp/Model/Task.swift
+++ ToDoApp/Model/Task.swift
@@ -9,25 +9,36 @@
 import Foundation
 
 struct Task {
-    let title				: String
-    let description			: String?
-    let location            : Location?
-    let date   				: Date
+    let title					: String
+    let description				: String?
+    let location            	: Location?
+    let date   					: Date
+	
+	var dict 					: [String : Any] {
+		var dict				: [String : Any] = [:]
+		dict["title"] = title
+		dict["description"]		= description
+		dict["date"]			= date
+		if let location			= location {
+			dict["location"]	= location.dict
+		}
+		return dict
+	}
     
 	init(title: String, description: String? = nil, location: Location? = nil, date: Date? = nil) {
-        self.title          = title
-        self.description    = description
-        self.location       = location
-        self.date           = date ?? Date()
+        self.title          	= title
+        self.description    	= description
+        self.location       	= location
+        self.date           	= date ?? Date()
     }
 }
 
 extension Task: Equatable {
 	static func == (lhs: Task, rhs: Task) -> Bool {
 		if
-			lhs.title		== rhs.title,
-			lhs.description	== rhs.description,
-			lhs.location	== rhs.location {
+			lhs.title			== rhs.title,
+			lhs.description		== rhs.description,
+			lhs.location		== rhs.location {
 			return true
 		}
 		return false
@@ -37,13 +48,13 @@ extension Task: Equatable {
 extension Task {
 	typealias PlistDictionary = [String : Any]
 	init?(dict: PlistDictionary) {
-		self.title			= dict["title"]			as! String
-		self.description	= dict["description"]	as? String
-		self.date			= dict["date"]			as? Date ?? Date()
-		if let locationDict	= dict["location"]		as? [String : Any] {
-			self.location	= Location(dict: locationDict)
+		self.title				= dict["title"]			as! String
+		self.description		= dict["description"]	as? String
+		self.date				= dict["date"]			as? Date ?? Date()
+		if let locationDict		= dict["location"]		as? [String : Any] {
+			self.location		= Location(dict: locationDict)
 		} else {
-			self.location	= nil
+			self.location		= nil
 		}
 	}
 }
--- ToDoAppTests/Model/LocationTests.swift
+++ ToDoAppTests/Model/LocationTests.swift
@@ -45,4 +45,12 @@ class LocationTests: XCTestCase {
 		let createdLocation = Location(dict: dict)
 		XCTAssertEqual(location, createdLocation)
 	}
+	
+	func testCanBeSerializedIntoDictionary() {
+		let location = Location(name: "Foo",
+								coordinate: CLLocationCoordinate2D(latitude: 10.0, longitude: 10.0))
+		let generatedLocation = Location(dict:location.dict)
+		
+		XCTAssertEqual(location, generatedLocation)
+	}
 }
